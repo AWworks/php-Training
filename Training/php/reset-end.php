e116132c67c72f853f6bf0d8423d08e07b682eaa
AbdulWahhab <dawahab@gmail.com>
2025-10-22 10:08:30 +0530

    Add Training/php directory structure

---
+++ Training/php/reset-end.php
@@ -0,0 +1,10 @@
+<?php
+// reset function gets the first member of the array.
+// end function gets the last member of the array.
+$odd_numbers = [1,3,5,7,9];
+$first_item = reset($odd_numbers);
+$last_item = end($odd_numbers);
+echo $first_item;
+echo $last_item;
+ 
+?>
