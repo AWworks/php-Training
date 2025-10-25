119bff8e69d5c8b145462236dd4347dd3d00b80c
AbdulWahhab <dawahab@gmail.com>
2025-10-25 10:32:17 +0530

    10/25 excer

---
+++ multi-array.php
@@ -0,0 +1,19 @@
+<?php
+$multiArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+print_r($multiArray[0][0]. "\n") ;
+
+//Associative multi-dimensional array
+$people = [
+    "John" => ["age" => 30, "city" => "New York"],
+    "Jane" => ["age" => 25, "city" => "Los Angeles"],
+    "faiz" => ["age" => 28, "city" => "Mangalore"],
+    "nadeem" => ["age" => 32, "city" => "Belgaum"]
+];
+
+print_r($people);
+?>
---
+++ sumarray.php
@@ -0,0 +1,28 @@
+<?php
+
+$numbers = [1, 2, 3, 4, 5];
+$sum = 0;
+foreach ($numbers as $number) {
+    $sum = $sum + $number;
+    echo "{$sum}\n";
+}
+
+
+// $matrix = [ 
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ];
+
+// foreach ($matrix as $numbers) {
+//     $sum = 0;
+//     foreach ($numbers as $number) {
+//         $sum += $number;
+//     }
+//     echo "{$sum}\n";
+// }
+
+
+
+
+?>
