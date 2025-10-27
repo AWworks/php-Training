c92d95ee35c4fbfe3386e77f5d4329e74f43b8a7
AbdulWahhab <dawahab@gmail.com>
2025-10-27 10:07:32 +0530

    string

---
+++ string.php
@@ -0,0 +1,14 @@
+<?php
+
+$course = "php - hypertext preprocessor";
+$hello = "I am learning ";
+// echo $hello." \n";
+
+echo $hello . "" . $course ."\n";
+echo "string length : " . strlen($course);
+echo "\n";
+
+$filename = "image.jpg";
+$extension = substr('image.jpg', 6);
+echo $extension ;
+?>
