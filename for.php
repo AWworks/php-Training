8ededa64bc9354d1fd3ab6aaef30fb2e46909162
AbdulWahhab <dawahab@gmail.com>
2025-10-28 10:20:20 +0530

    loops

---
+++ for.php
@@ -0,0 +1,11 @@
+<?php
+
+$odd_numbers = [3, 5, 7, 9, 11, 13, 15, 17, 19, 21];
+
+// for ($i=0; $i < count($odd_numbers); $i++) { 
+//     echo $odd_numbers[$i] . "\n";
+// }
+
+foreach ($odd_numbers as $number) {
+    echo $number . "\n";
+}
---
+++ foreach.php
@@ -0,0 +1,12 @@
+<?php
+
+$phoneNumber = [
+    "Ahmed" => "9203938301",
+    "Sajid" => "9203938302",
+    "Salman" => "9203938303",   
+    "Riyan" => "9203938304",
+    "Rashid" => "9203938305"  
+];
+foreach ($phoneNumber as $name => $number) {
+    echo "The phone number of $name is $number \n";
+}
