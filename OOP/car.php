ca761d5cd61b5af0f104d7a9cbd048e880a578d7
AbdulWahhab <dawahab@gmail.com>
2025-11-13 10:01:50 +0530

    methods

--- OOP/car.php
+++ OOP/car.php
@@ -20,6 +20,22 @@ class Car
     {
         echo "Car: " . $this->year . " " . $this->make . " " . $this->model . " " . $this->color . "\n";
     }
+    public function start()
+    {
+        echo $this->make . " " . $this->model . " has started.\n";
+    }
+    public function applyBrakes()
+    {
+        echo $this->make . " " . $this->model . "The brakes have been applied.\n";
+    }
+    public function stop()
+    {
+        echo $this->make . " " . $this->model . "The car has stopped.\n";
+    }
+    public function accelerate($speed)
+    {
+        echo "The car is accelerating to " . $speed . " km/h.\n";
+    }
 }
 
 // Creating an instance of the Car class
@@ -27,5 +43,10 @@ $myCar = new Car("Toyota", "Corolla", 2020, "Blue");
 $myCar->displayDetails();
 $myCar2 = new Car("Honda", "City", 2023, "White");
 $myCar2->displayDetails();
+
+$myCar->start();
+$myCar->accelerate(60);
+$myCar->applyBrakes();
+$myCar->stop();
 
 ?>
